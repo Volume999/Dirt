c15cbe28e73666765a14b458652549579c6a18de
Dosya <alybaev_d@auca.kg>
2018-11-20 22:17:50 +0600

    Map saves locations in markers table, but it does not show locations

--- viewPlacesMap.php
+++ viewPlacesMap.php
@@ -7,46 +7,46 @@ require_once ("config.php");
 	}
 	
 	
-	$result = mysqli_query($conn,"SELECT lat, lng FROM coordinates");   
+	$result = mysqli_query($conn,"SELECT lat, lng FROM markers");   
 	while($row = mysqli_fetch_assoc( $result)){
    	  $json[] = $row;
  	}
 
  	$json_encoded = json_encode($json,JSON_NUMERIC_CHECK	);
  	
- 	$html = "<!DOCTYPE html>
-<html>
-  <head>
-    <meta name='viewport' content='initial-scale=1.0, user-scalable=no'>
-    <meta charset='utf-8'>
-    <title>Marker Clustering</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-  </head>
-  <body>
-    <div id='map'></div>
-    <script  type='text/javascript'>
+   	$html = "<!DOCTYPE html>
+  <html>
+    <head>
+      <meta name='viewport' content='initial-scale=1.0, user-scalable=no'>
+      <meta charset='utf-8'>
+      <title>Marker Clustering</title>
+      <style>
+        /* Always set the map height explicitly to define the size of the div
+         * element that contains the map. */
+        #map {
+          height: 100%;
+        }
+        /* Optional: Makes the sample page fill the window. */
+        html, body {
+          height: 100%;
+          margin: 0;
+          padding: 0;
+        }
+      </style>
+    </head>
+    <body>
+      <div id='map'></div>
+      <script  type='text/javascript'>
 
-      function initMap() {
+        function initMap() {
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: {lat: 42.8640117, lng: 74.5460088	}
-        });
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: {lat: 42.8640117, lng: 74.5460088	}
+          });
 
-        // Create an array of alphabetical characters used to label the markers.
-        var labels = 'DASTAN';
+          // Create an array of alphabetical characters used to label the markers.
+          var labels = 'DASTAN';
 
       
         var markers = locations.map(function(location, i) {
